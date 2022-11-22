62f59185ac11add87aec51e40769bec4cd4d21e2
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-22 20:23:13 +0530

    Add files via upload

---
+++ functionsasdm.js
@@ -0,0 +1,22 @@
+/* 
+  TODO: Write a function that sums two numbers
+  TODO: Write a function that subtracts two numbers
+  TODO: Write a function that divides two numbers.
+  TODO: Write a function that multiplies two numbers.
+  *NOTE* be sure to handle dividing by zero 😉
+  ES5 Syntax: function Add(){}
+  ES6 Syntax (Arrow function): const add = () => {}
+*/
+let add = (a, b) => a + b;
+let sub = (c, d) => c + d;
+let divide = (e, f) => e / f;
+let multiply = (g, h) => g * h;
+
+console.log(add(10, 20))
+console.log(sub(20, 10))
+console.log(divide(20, 10))
+console.log(multiply(20, 10))
+
+/*
+  TODO: create a function that console logs the result of any of the above operations.
+*/
---
+++ templateliterals.js
@@ -0,0 +1,6 @@
+function greet(name){
+    console.log(`Hi! ${name}. Hope you are doing well!`)
+  }
+  greet('Aathi')
+  greet('Haris')
+  
