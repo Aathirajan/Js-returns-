69ba32838f93a7adc162ef56576c60014de0b232
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-23 09:57:45 +0530

    Add files via upload

---
+++ objectsplay.js
@@ -0,0 +1,18 @@
+
+
+
+let person = { name: 'aathi', Age: '24' }
+
+// console.log(person.name)
+// console.log(person.Age)
+
+person.phone = '9047540800'
+// console.log(person)
+
+person['mother'] = 'Fathima'
+console.log(person)
+
+let person2 = { name: 'qazi', Age: '28' }
+console.log(person2)
+person2.phone = '1-222-555-444'
+person2.mother = 'Pazi'
