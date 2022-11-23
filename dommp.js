1e52f2ef247ae70d739b70149fcabf93a6d92f59
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-23 20:13:51 +0530

    Add files via upload

---
+++ dommp.js
@@ -0,0 +1,7 @@
+console.log('hello world')
+
+let hitext= document.getElementById('hitext')
+console.log(hitext.innerText)
+
+hitext.innerText='Hello world its from DOM'
+console.log(hitext.innerText)
