adb50473f3dabeb8e88d2b8b24d30972eb7a717f
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-23 09:55:42 +0530

    Add files via upload

---
+++ playing with array.js
@@ -0,0 +1,19 @@
+// Arrays:
+
+// practicing array indexing
+let items = ['1', '2', '3', '4']
+
+// console.log(items)
+// console.log(items[0])
+// console.log(items[1])
+// console.log(items[2])
+
+// we tryna push 5 into the items array
+items.push('5')
+console.log(items)
+
+// we are gonna slice the array from 4th index to 8th element
+
+console.log(items.slice(4, 8))
+
+console.log(items.length)
