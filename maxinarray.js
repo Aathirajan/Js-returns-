3352a674efe264b832eeb53da67257447f159bdb
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-24 11:48:10 +0530

    Add files via upload

---
+++ maxinarray.js
@@ -0,0 +1,17 @@
+
+// Write a function that takes in an array of numbers and returns the largest number
+
+function findMax(array) {
+    result = 0
+    for (number of array) {
+        if (number > result) {
+            result = number
+        } else {
+            return result
+        }
+
+    }
+    console.log(result)
+}
+console.log(findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 99]))
+  //Topics: loops, arrays, conditions,
