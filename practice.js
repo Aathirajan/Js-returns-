845d88413c082e019bd8a33b3a0395d4024433c1
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-11-29 22:19:33 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,24 @@
+// let fn= (numarray)=>{
+//     let results=0;
+//      for(let i of numarray){
+//        console.log(i)
+//        results+=i;
+//        }
+//       return {results};
+//      }
+//  x=[1,2,3,4,5]
+//  console.log(fn(x));
+ 
+// let x=(array)=>{
+//   let result=0;
+//  for(let i of array)
+//     {
+//       result +=i;
+//       console.log(result)
+//     }
+//   return {result};
+//   }
+
+
+// let y=[1,2,3,4,5,6,7,8,9]
+// console.log(x(y));
